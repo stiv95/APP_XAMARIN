ceb1dcbb6a470b5f60128a8002cb77522a2bbbb1
stiv95 <stivenmoar@gmail.com>
2023-07-24 16:33:50 -0500

    secund commit

--- Mobile_App_Estiven/Mobile_App_Estiven/ViewModels/LoginViewModel.cs
+++ Mobile_App_Estiven/Mobile_App_Estiven/ViewModels/LoginViewModel.cs
@@ -8,7 +8,77 @@ namespace Mobile_App_Estiven.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private string _username;
+        private string _password;
+        private bool _showMessage;
+        private string _welcomeMessage;
+        private Color _messageColor;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (_username != value)
+                {
+                    _username = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public Color MessageColor
+        {
+            get => _messageColor;
+            set
+            {
+                if (_messageColor != value)
+                {
+                    _messageColor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public bool ShowMessage
+        {
+            get => _showMessage;
+            set
+            {
+                if (_showMessage != value)
+                {
+                    _showMessage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string WelcomeMessage
+        {
+            get => _welcomeMessage;
+            set
+            {
+                if (_welcomeMessage != value)
+                {
+                    _welcomeMessage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public Command LoginCommand { get; }
+
+        public string UserName { get; set; }
+
+
 
         public LoginViewModel()
         {
@@ -17,8 +87,33 @@ namespace Mobile_App_Estiven.ViewModels
 
         private async void OnLoginClicked(object obj)
         {
-            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            if (ValidateFiels())
+            {
+                //await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+                if (Username == "stivmoar" && Password == "1234567")
+                {
+                    WelcomeMessage = "Inicio de sesión exitoso"+ Environment.NewLine + "¡Bienvenido! " + Username;
+                    MessageColor = Color.Green;
+                    await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+                }
+                else
+                {
+                    ShowMessage = true;
+                    MessageColor = Color.Red;
+                    WelcomeMessage = "Usuario o contraseña incorrectos";
+                }
+            }
+        }
+        private bool ValidateFiels()
+        {
+            if (!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
     }
